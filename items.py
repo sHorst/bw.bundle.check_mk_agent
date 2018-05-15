4e78cb169fd64df9c9977815fb328660b8c87c73
Stefan Horst <stefan.horst@grandcentrix.net>
2018-05-15 16:38:06 +0200

    added stable version

--- items.py
+++ items.py
@@ -1,9 +1,14 @@
 
-CHECK_MK_AGENT_VERSION = '1.5.0b1-1'
-CHECK_MK_AGENT_SHA256 = 'af23d928e3aadc23382846e728effc6d93d14f9e76639d0833143216cafd125c'
 
 check_mk_config = node.metadata.get('check_mk', {})
 check_mk_server = repo.get_node(check_mk_config.get('server', ''))
+
+if check_mk_server.metadata.get('check_mk', {}).get('beta', False):
+    CHECK_MK_AGENT_VERSION = '1.5.0b3-1'
+    CHECK_MK_AGENT_SHA256 = 'd14b2ef6babcc9f5b36968661cf3106acdcc667f21d954a34adf870d50ceb43c'
+else:
+    CHECK_MK_AGENT_VERSION = '1.4.0p31-1'
+    CHECK_MK_AGENT_SHA256 = 'fb3aacd46e79b15acef947fb390ca678b4f9ad1a1165db4ba0bcff7e5800e51f'
 
 svc_systemd = {
     'xinetd': {
@@ -21,10 +26,12 @@ pkg_apt = {
 
 downloads = {
     '/tmp/check-mk-agent_{}_all.deb'.format(CHECK_MK_AGENT_VERSION): {
-        'url': 'https://{server}/prod/check_mk/agents/check-mk-agent_{version}_all.deb'.format(
+        'url': 'https://{server}/{site}/check_mk/agents/check-mk-agent_{version}_all.deb'.format(
             server=check_mk_server.hostname,
+            site=list(check_mk_server.metadata.get('check_mk', {}).get('sites', {}).keys())[0],
             version=CHECK_MK_AGENT_VERSION
         ),
+        'verifySSL': False,
         'sha256': CHECK_MK_AGENT_SHA256,
         'unless': 'dpkg -l | grep check-mk-agent | grep -q {version}'.format(version=CHECK_MK_AGENT_VERSION)
     }
